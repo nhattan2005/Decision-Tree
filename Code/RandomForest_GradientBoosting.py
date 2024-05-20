e9de1990d3306518a3d6f85a1a78b722d2b0d64e
Phan Nhật Tân <162537750+nhattan2005@users.noreply.github.com>
2024-05-20 20:24:49 +0700

    Update RandomForest_GradientBoosting.py

--- Code/RandomForest_GradientBoosting.py
+++ Code/RandomForest_GradientBoosting.py
@@ -1,4 +1,4 @@
-# Khai báo thư viện
+""" Khai báo thư viện """
 import numpy as np
 import pandas as pd
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
@@ -8,35 +8,35 @@ import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier,AdaBoostClassifier,GradientBoostingClassifier
 
 
-# Hàm tách các thuộc tính và biến mục tiêu
+""" Hàm tách các thuộc tính và biến mục tiêu """
 def splitdataset(balance_data):
-    # Biến X chứa các thuộc tính
+    """ Biến X chứa các thuộc tính """
     X = balance_data.values[:, 1:5]    
-    # Biến Y chứa biến mục tiêu
+    """ Biến Y chứa biến mục tiêu """
     Y = balance_data.values[:, 0]
 
-    # Chia bộ dữ liệu thành training và testing dataset với tỉ lệ 70:30 (70% cho training, 30% cho test)
+    """ Chia bộ dữ liệu thành training và testing dataset với tỉ lệ 70:30 (70% cho training, 30% cho test) """
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=100)
 
     return X, Y, X_train, X_test, y_train, y_test
 
 
-# Hàm tính toán độ chính xác của dự đoán 
+""" Hàm tính toán độ chính xác của dự đoán """
 def cal_accuracy(y_test, y_pred):
     print("Confusion Matrix: ", confusion_matrix(y_test, y_pred))
     print("Accuracy : ", accuracy_score(y_test, y_pred)*100)
     print("Report : ", classification_report(y_test, y_pred, zero_division=1))
 
 
-# Import dataset
+""" Import dataset """
 data = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/balance-scale/balance-scale.data',
         sep=',', header=None)
 
-# Chia dataset thành training và test set
+""" Chia dataset thành training và test set """
 X, Y, X_train, X_test, y_train, y_test = splitdataset(data)
 
 
-# Mô hình Random Forest
+""" Mô hình Random Forest """
 clf = RandomForestClassifier(n_estimators=100, max_features="sqrt", random_state=0)
 clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
@@ -44,7 +44,7 @@ print("Random Forest:")
 cal_accuracy(y_test, y_pred)
 
 
-# Mô hình Gradient Boosting
+""" Mô hình Gradient Boosting """
 clf = GradientBoostingClassifier(n_estimators=100)
 clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
