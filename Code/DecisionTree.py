f7722313416d4e090082df05d6dd0c55f1353f81
Phan Nhật Tân <162537750+nhattan2005@users.noreply.github.com>
2024-05-20 20:18:51 +0700

    Update DecisionTree.py

--- Code/DecisionTree.py
+++ Code/DecisionTree.py
@@ -1,4 +1,4 @@
-# Khai báo các thư viện cần thiết
+""" Khai báo các thư viện cần thiết """
 import numpy as np
 import pandas as pd
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
@@ -6,13 +6,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 import matplotlib.pyplot as plt
 
-# Hàm import dataset
+""" Hàm import dataset """
 def importdata():
     balance_data = pd.read_csv(
         'https://archive.ics.uci.edu/ml/machine-learning-databases/balance-scale/balance-scale.data',
         sep=',', header=None)
 
-    # Biểu diễn thông tin của dataset
+    """ Biểu diễn thông tin của dataset """
     print("Dataset Length: ", len(balance_data))
     print("Dataset Shape: ", balance_data.shape)
     print("Dataset: ", balance_data.head())
@@ -20,30 +20,30 @@ def importdata():
     return balance_data
 
 
-# Hàm tách các thuộc tính và biến mục tiêu
+""" Hàm tách các thuộc tính và biến mục tiêu """
 def splitdataset(balance_data):
-    # Biến X chứa các thuộc tính
+    """ Biến X chứa các thuộc tính """
     X = balance_data.values[:, 1:5]    
-    # Biến Y chứa biến mục tiêu
+    """ Biến Y chứa biến mục tiêu """
     Y = balance_data.values[:, 0]
 
-    # Chia bộ dữ liệu thành training và testing dataset với tỉ lệ 70:30 (70% cho training, 30% cho test)
+    """ Chia bộ dữ liệu thành training và testing dataset với tỉ lệ 70:30 (70% cho training, 30% cho test) """
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=100)
 
     return X, Y, X_train, X_test, y_train, y_test
 
 
-# Hàm train sử dụng Gini Index
+""" Hàm train sử dụng Gini Index """
 def train_using_gini(X_train, X_test, y_train):
-    # Tạo đối tượng phân loại
+    """ Tạo đối tượng phân loại """
     clf_gini = DecisionTreeClassifier(criterion="gini", random_state=100, max_depth=3, min_samples_leaf=5)
 
-    # Training
+    """ Training """
     clf_gini.fit(X_train, y_train)
     return clf_gini
 
 
-# Hàm dự đoán
+""" Hàm dự đoán """
 def prediction(X_test, clf_object):
     y_pred = clf_object.predict(X_test)
     print("Predicted values:")
@@ -51,7 +51,7 @@ def prediction(X_test, clf_object):
     return y_pred
 
 
-# Hàm tính toán độ chính xác của dự đoán
+""" Hàm tính toán độ chính xác của dự đoán """
 def cal_accuracy(y_test, y_pred):
     print("Confusion Matrix: ", confusion_matrix(y_test, y_pred))
     print("Accuracy : ", accuracy_score(y_test, y_pred)*100)
@@ -59,7 +59,7 @@ def cal_accuracy(y_test, y_pred):
 
 
 
-# Hàm vẽ decision tree
+""" Hàm vẽ decision tree """
 def plot_decision_tree(clf_object, feature_names, class_names):
     plt.figure(figsize=(15, 10))
     plot_tree(clf_object, filled=True, feature_names=feature_names, class_names=class_names, rounded=True)
@@ -72,7 +72,7 @@ if __name__ == "__main__":
 
     clf_gini = train_using_gini(X_train, X_test, y_train)
 
-    # Trực quan hóa Decision Trees
+    """ Trực quan hóa Decision Trees """
     plot_decision_tree(clf_gini, ['X1', 'X2', 'X3', 'X4'], ['L', 'B', 'R'])
 
     print("Results Using Gini Index:")
